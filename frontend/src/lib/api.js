12eac9aa0dbf87ea2fb593844390b0e7ed057dbc
sudarshankillekar <54753652+sudarshankillekar@users.noreply.github.com>
2026-04-14 00:29:22 +0530

    Create api.js for Axios instance with token interceptor

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { BACKEND_URL } from "./config";
+
+const api = axios.create({
+  baseURL: BACKEND_URL,
+});
+
+// 🔥 Automatically attach token to every request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
